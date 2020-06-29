861475307981d51d663eb83b39170112fa6d6b29
yulia <juliana.ny2008@gmail.com>
2020-06-29 10:47:38 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,6 @@
+
 Dir[File.join(File.dirname(__FILE__), "../lib", "*.rb")].each {|f| require f}
+
+
+
+ 
---
+++ lib/new.rb
@@ -0,0 +1,11 @@
+# require_relative '../config/environment.rb'
+# def reload
+#   load 'config/environment.rb'
+# end 
+
+# # y=Dog.new("k", "me")
+# # t=Owner.new("luk")
+
+# binding.pry 
+# 0
+
