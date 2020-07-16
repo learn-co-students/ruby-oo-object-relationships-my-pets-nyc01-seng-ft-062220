3af6fd1b59263340447eca8ab3b92f894f3ad95c
junko911 <junkotahara911@gmail.com>
2020-07-16 10:45:49 -0400

    Added 4 methods

--- lib/owner.rb
+++ lib/owner.rb
@@ -23,4 +23,20 @@ class Owner
   def self.reset_all
     @@all.clear
   end
+
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_cat(cat_name)
+    Cat.new(cat_name, self)
+  end
+
+  def buy_dog(dog_name)
+    Dog.new(dog_name, self)
+  end
 end
